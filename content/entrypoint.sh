8c4fb782c88b2d5a3a677303ea297ce33a7a06bc
wy580477 <98247050+wy580477@users.noreply.github.com>
2022-04-16 13:25:33 +0800

    Update entrypoint.sh

--- content/entrypoint.sh
+++ content/entrypoint.sh
@@ -1,14 +1,68 @@
 #!/bin/sh
 
-if [ "${PUID}" = "0" ]; then
-    chown -R ${PUID} /etc/service
-    ln -s /.aria2allinoneworkdir/service/* /etc/service/ 2>/dev/null
-    exec runsvdir -P /etc/service
-else
-    mkdir -p /mnt/data/downloads /mnt/data/finished 2>/dev/null
-    rm -rf /etc/service/*
-    chown -R ${PUID}:${PGID} /.aria2allinoneworkdir /mnt/config 2>/dev/null
-    chown ${PUID}:${PGID} /mnt/data /mnt/data/downloads /mnt/data/finished /etc/service
-    su-exec ${PUID}:${PGID} ln -s /.aria2allinoneworkdir/service/* /etc/service/ 2>dev/null
-    exec su-exec ${PUID}:${PGID} runsvdir -P /etc/service
+# Dummy run for building image
+if [ "${MODE}" = "build" ]; then
+       sleep infinity
 fi
+
+if [ "${PUID}" != "0" ]; then
+    mkdir -p /mnt/data/downloads /mnt/data/finished 2>/dev/null
+    chown -R ${PUID}:${PGID} /workdir /mnt/config 2>/dev/null
+    chown ${PUID}:${PGID} /mnt/data /mnt/data/downloads /mnt/data/finished
+fi
+
+# Configure aria2
+if [ ! -d "/mnt/config/aria2" ]; then
+       mkdir -p /mnt/config/aria2
+       cp /workdir/aria2/*.conf /mnt/config/aria2/
+       cp /workdir/aria2/*.dat /mnt/config/aria2/
+       cp /workdir/aria2/tracker.sh /mnt/config/aria2/
+fi
+
+if [ ! -f "/mnt/config/aria2/aria2.session" ]; then
+       touch /mnt/config/aria2/aria2.session
+fi
+
+cp /mnt/config/aria2/aria2.conf /workdir/aria2.conf
+sed -i "s|^rpc-secret=.*|rpc-secret=${PASSWORD}|g" /workdir/aria2.conf
+
+if [ "${ARIA_IPV6}" = "true" ]; then
+       sed -i "s|^disable-ipv6=.*|disable-ipv6=false|g" /workdir/aria2.conf
+       sed -i "s|^enable-dht6=.*|enable-dht6=true|g" /workdir/aria2.conf
+else
+       sed -i "s|^disable-ipv6=.*|disable-ipv6=true|g" /workdir/aria2.conf
+       sed -i "s|^enable-dht6=.*|enable-dht6=false|g" /workdir/aria2.conf
+fi
+
+if [ "${AUTO_DRIVE_NAME}" = "true" ]; then
+       DRIVE_NAME_AUTO="$(sed -n '1p' /mnt/config/rclone.conf | sed "s/\[//g" | sed "s/\]//g")"
+       sed -i "s|^drive-name=.*|drive-name=${DRIVE_NAME_AUTO}|g" /mnt/config/aria2/script.conf
+fi
+
+if [ "${POST_MODE}" = "copy_remote_first" ]; then
+       sed -i "s|^on-bt-download-complete=/workdir/aria2/.*.sh|on-bt-download-complete=/workdir/aria2/"${POST_MODE}".sh|g" /workdir/aria2.conf
+       sed -i "s|^on-download-complete=/workdir/aria2/.*.sh|on-download-complete=/workdir/aria2/copy_remote_other.sh|g" /workdir/aria2.conf
+elif [ "${POST_MODE}" = "dummy" ]; then
+       sed -i "s|^on-bt-download-complete=/workdir/aria2/.*.sh|on-bt-download-complete=/workdir/aria2/clean.sh|g" /workdir/aria2.conf
+       sed -i "s|^on-download-complete=/workdir/aria2/.*.sh|on-download-complete=/workdir/aria2/clean.sh|g" /workdir/aria2.conf
+elif [ "${POST_MODE}" = "custom" ]; then
+       :
+else
+       sed -i "s|^on-download-complete=/workdir/aria2/.*.sh|on-download-complete=/workdir/aria2/"${POST_MODE}".sh|g" /workdir/aria2.conf
+       sed -i "s|^on-bt-download-complete=/workdir/aria2/.*.sh|on-bt-download-complete=/workdir/aria2/clean.sh|g" /workdir/aria2.conf
+fi
+
+if [[ "${CLEAN_UNFINISHED_FAILED_TASK_FILES}" = "true" ]] && [[ "${POST_MODE}" != "custom" ]]; then
+       sed -i "s|^on-download-stop=/workdir/aria2/.*.sh|on-download-stop=/workdir/aria2/delete.sh|g" /workdir/aria2.conf
+elif [ "${POST_MODE}" = "custom" ]; then
+       :
+else
+       sed -i "s|^on-download-stop=/workdir/aria2/.*.sh|on-download-stop=/workdir/aria2/clean.sh|g" /workdir/aria2.conf
+fi
+
+# Run aria2
+exec su-exec ${PUID}:${PGID} aria2c --conf-path="/workdir/aria2.conf" \
+--enable-rpc \
+--dht-listen-port=${BT_PORT} \
+--listen-port=${BT_PORT} \
+--rpc-listen-port=${RPC_PORT} $@
